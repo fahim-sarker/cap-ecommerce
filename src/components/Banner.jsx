8118082ce1e9328a78b525a8b0a89efad515defb
fahim-sarker <157319749+fahim-sarker@users.noreply.github.com>
2024-11-22 07:34:21 +0300

    set categoy from api

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -15,63 +15,69 @@ const Banner = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
-    dots : false,
+    dots: false,
     autoplay: true,
     speed: 3000,
     autoplaySpeed: 2000,
   };
 
-  let capdata = useContext(apidata)
-  let [category, setCategory] = useState ();
+  let capdata = useContext(apidata);
+  let [category, setCategory] = useState();
 
-  useEffect(()=>{
-    setCategory([...new Set(capdata.map((item)=>item.category))])
-  })
+  useEffect(() => {
+    setCategory([...new Set(capdata.map((item) => item.category))]);
+  });
+  console.log(category);
 
   return (
     <div className="bg-[#f8f9fa]">
       <div className="container mx-auto">
         <div className="flex justify-between">
           <div className="w-[20%] pt-10 pr-5 bg-[#f8f9fa] lg:block hidden">
+            <ul className="cursor-pointer">
+              {category?.map((item) => (
+                <li className="flex justify-between capitalize items-center font-roboto text-[18px] font-normal py-5 hover:text-sky-400 duration-300 ease-in-out px-2">
+                  {item}
+                  <MdArrowForwardIos />
+                </li>
+              ))}
+              <li className="flex justify-between items-center font-roboto text-[18px] font-normal py-5 hover:text-sky-400 duration-300 ease-in-out px-2">
+                Electronics & Appliances
+                <MdArrowForwardIos />
+              </li>
+              <li className="flex justify-between items-center font-roboto text-[18px] font-normal py-5 hover:text-sky-400 duration-300 ease-in-out px-2">
+                Mobile Accessories
+                <MdArrowForwardIos />
+              </li>
 
-          <ul className="cursor-pointer">
-            <li className="flex justify-between items-center font-roboto text-[18px] font-normal py-5 hover:text-sky-400 duration-300 ease-in-out px-2">
-              {category}
-              <MdArrowForwardIos />
-            </li>
-            {/* <li className="flex justify-between items-center font-roboto text-[18px] font-normal py-5 hover:text-sky-400 duration-300 ease-in-out px-2">
-              Electronics & Appliances
-              <MdArrowForwardIos />
-            </li>
-            <li className="flex justify-between items-center font-roboto text-[18px] font-normal py-5 hover:text-sky-400 duration-300 ease-in-out px-2">
-              Mobile Accessories
-              <MdArrowForwardIos />
-            </li>
-            <li className="flex justify-between items-center font-roboto text-[18px] font-normal py-5 hover:text-sky-400 duration-300 ease-in-out px-2">
-              Computers
-              <MdArrowForwardIos />
-            </li>
-            <li className="flex justify-between items-center font-roboto text-[18px] font-normal py-5 hover:text-sky-400 duration-300 ease-in-out px-2">
-              Computer Accessories
-              <MdArrowForwardIos />
-            </li>
-            <li className="flex justify-between items-center font-roboto text-[18px] font-normal py-5 hover:text-sky-400 duration-300 ease-in-out px-2">
-              Lifestyle
-              <MdArrowForwardIos />
-            </li> */}
-          </ul>
- 
+              <li className="flex justify-between items-center font-roboto text-[18px] font-normal py-5 hover:text-sky-400 duration-300 ease-in-out px-2">
+                Computers
+                <MdArrowForwardIos />
+              </li>
+            </ul>
           </div>
           <div className="lg:w-[80%] w-full">
             <Slider {...settings}>
               <div className="">
-                <img src={Ban} alt="Banner" className="lg:h-[500px] h-[200px]" />
+                <img
+                  src={Ban}
+                  alt="Banner"
+                  className="lg:h-[500px] h-[200px]"
+                />
               </div>
               <div className="">
-                <img src={Ban1} alt="Banner" className="lg:h-[500px] h-[200px] w-full" />
+                <img
+                  src={Ban1}
+                  alt="Banner"
+                  className="lg:h-[500px] h-[200px] w-full"
+                />
               </div>
               <div className="">
-                <img src={Ban} alt="Banner" className="lg:h-[500px] h-[200px]" />
+                <img
+                  src={Ban}
+                  alt="Banner"
+                  className="lg:h-[500px] h-[200px]"
+                />
               </div>
             </Slider>
           </div>
